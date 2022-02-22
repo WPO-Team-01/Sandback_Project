827412829a921cbb626924f16d3d52480be1b7fc
김범석 <kqjatjr@gmail.com>
2022-02-22 22:09:56 +0900

    contentApi query 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { contentApi } from "./query/contentApi";
 
 export const store = configureStore({
+  reducer: {
+    [contentApi.reducerPath]: contentApi.reducer,
+  },
   devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(contentApi.middleware),
 });
 
 setupListeners(store.dispatch);
