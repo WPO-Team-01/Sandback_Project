7cf87f5870f36c0c33b1408d9dee3adefce1512b
Yongrok <94chl@naver.com>
2022-02-23 15:11:12 +0900

    refactor: 더미데이터 삭제 & rtk query 적용

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -1,52 +1,24 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
+import { useGetContentsQuery as UseGetContentsQuery } from "../../store/query/contentApi";
 
-const dummyArr = [
-  {
-    id: 63,
-    sector_id: 1,
-    title: "디지털 위안화 나오면 비트코인 더 필요해진다",
-    body: null,
-    image:
-      "https://sandbank-image.s3.ap-northeast-2.amazonaws.com/info/bjob_024.png",
-    link: "https://medium.com/sandbank-kr/46cd56322be6",
-    upload_date: "2022-02-16",
-    like_cnt: 0,
-    like_top: 1,
-  },
-  {
-    id: 59,
-    sector_id: 1,
-    title: "왜 주식과 비트코인은 함께 떨어질까",
-    body: null,
-    image:
-      "https://sandbank-image.s3.ap-northeast-2.amazonaws.com/info/bjob_023.png",
-    link: "https://medium.com/sandbank-kr/c7b31c05301b",
-    upload_date: "2022-01-29",
-    like_cnt: 0,
-    like_top: 1,
-  },
-  {
-    id: 58,
-    sector_id: 1,
-    title: "[칼럼] 비트코인 도미넌스 바로알기",
-    body: null,
-    image:
-      "https://sandbank-image.s3.ap-northeast-2.amazonaws.com/info/bjob_022.png",
-    link: "https://medium.com/sandbank-kr/946d87433d4",
-    upload_date: "2022-01-21",
-    like_cnt: 0,
-    like_top: 1,
-  },
-];
-
-const Carousel = ({ carouselContents }) => {
-  const [contents, setContents] = useState(dummyArr);
+const Carousel = ({ carouselContents, sectorId = 1 }) => {
+  const { data } = UseGetContentsQuery();
+  const [contents, setContents] = useState([]);
   const [activeIndex, setActiveIndex] = useState(null);
+
+  useEffect(() => {
+    if (data?.ok) {
+      const newContents = data.content.filter(
+        (article) => article.like_top === 1 && article.sector_id === sectorId
+      );
+      setContents(newContents);
+    }
+  }, [data]);
 
   const changeActiveIndex = (e) => {
     const activeIndex =
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
-  content: [],
-=======
   content: ["./src/**/*.{js}"],
->>>>>>> b3024d2b04c1e5d6cab019acf73b72121da388ed
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> b3024d2b04c1e5d6cab019acf73b72121da388ed
