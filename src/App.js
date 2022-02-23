b944786c0c5bed5d5f4cb9ac3221948950918e38
Yongrok <94chl@naver.com>
2022-02-23 21:58:57 +0900

    test: carousel 오류 확인

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import Tabs from "./components/Tabs/Tabs";
 import Carousel from "./components/Carousel/Carousel";
 import Subscribe from "./components/Subscribe/Subscribe";
-import MainList from "./components/MainList/MainList";
+// import MainList from "./components/MainList/MainList";
 
 function App() {
   return (
     <div className="App">
-      <div className='max-w-6xl mx-auto'>
+      <div className="max-w-6xl mx-auto">
         <Tabs />
         <Carousel />
         <Subscribe />
-        <MainList />
+        {/* <MainList /> */}
       </div>
     </div>
   );
