73edf486199f47a6328af5d32177198e5c6ee5e5
jeewon-bang <giw0233@gmail.com>
2022-02-23 20:41:06 +0900

    fix : props 설정

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -2,19 +2,24 @@ import React from "react";
 import { useGetContentsQuery } from "../../store/query/contentApi";
 import { MdOutlineFileUpload } from "react-icons/md";
 
-const Share = () => {
-  const { data, error, isLoading } = useGetContentsQuery();
+const Share = (props) => {
+  const { data } = props;
+  const { isLoading } = useGetContentsQuery();
 
   return (
     <div className="flex justify-center items-center text-slate-400">
       {!isLoading && (
         <button
           onClick={() =>
-            window.open(`${data.content[0].link}`, "Popup", "_blank")
+            window.open(
+              `${data.content[data.content.id].link}`,
+              "Popup",
+              "_blank"
+            )
           }
           className="flex justify-center items-center "
         >
-          <MdOutlineFileUpload className="mr-2" />
+          <MdOutlineFileUpload className="mr-1" />
           공유하기
         </button>
       )}
