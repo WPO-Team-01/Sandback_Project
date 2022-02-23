fa5635c6b5ceaac2bac2350f704c576a6110e0b0
jeewon-bang <giw0233@gmail.com>
2022-02-23 18:07:49 +0900

    add react-icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import "./App.css";
-import Like from "./components/Like/Like";
-import Share from "./components/Share/Share";
 
 function App() {
-  return (
-    <div className="App">
-      <Like />
-      <Share />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
