e57015d34234052134dea14f816a6c350382deb2
Yongrok <94chl@naver.com>
2022-02-23 14:01:50 +0900

    add: Carousel 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import Carousel from "./component/Carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
+      <Carousel></Carousel>
     </div>
   );
 }
--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const dummyArr = [
   {
@@ -41,14 +46,53 @@ const dummyArr = [
 
 const Carousel = ({ carouselContents }) => {
   const [contents, setContents] = useState(dummyArr);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const changeActiveIndex = (e) => {
+    const activeIndex =
+      e.activeIndex > contents.length
+        ? 1
+        : e.activeIndex < 1
+        ? contents.length
+        : e.activeIndex;
+    setActiveIndex(activeIndex);
+  };
 
   return (
     <div>
-      <ul>
-        {contents.map((content) => (
-          <li>{content.title}</li>
+      <Swiper
+        slidesPerView={1}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        spaceBetween={30}
+        speed={1000}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className={"mySwiper"}
+        style={{}}
+        onActiveIndexChange={(e) => changeActiveIndex(e)}
+      >
+        {contents.map((content, index) => (
+          <SwiperSlide key={`${content.id}-${index}`} className="flex flex-row">
+            <div>
+              <a href={content.link} target="_blank" rel="noreferrer">
+                <img src={content.image} alt={`${content.title}_img`} />
+              </a>
+            </div>
+            <div>
+              <a href={content.link} target="_blank" rel="noreferrer">
+                <div>{content.title}</div>
+              </a>
+            </div>
+          </SwiperSlide>
         ))}
-      </ul>
+      </Swiper>
     </div>
   );
 };
